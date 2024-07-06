6cc64ed0f802861c03149307a12ce412ff957d22
proteinrobot <proteinrobot@pm.me>
2024-07-07 04:50:50 +0800

    Fix hostname and client id
    Signed-off-by: proteinrobot <proteinrobot@pm.me>

--- src/integ_tests/dhcpv4.rs
+++ src/integ_tests/dhcpv4.rs
@@ -1,25 +1,83 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{DhcpV4Client, DhcpV4Config, DhcpV4Lease};
+use crate::{
+    integ_tests::env::get_test_veth_cli_mac, DhcpV4Client, DhcpV4Config,
+    DhcpV4Lease,
+};
 
 use super::env::{DhcpServerEnv, FOO1_STATIC_IP, TEST_NIC_CLI};
 
 const POLL_WAIT_TIME: u32 = 5;
 
 #[test]
-fn test_dhcpv4_manual_client_id() {
+fn test_dhcpv4_get_ip() {
     let _srv = DhcpServerEnv::start();
 
     let mut config = DhcpV4Config::new(TEST_NIC_CLI);
     config.set_host_name("foo1");
-    config.use_host_name_as_client_id();
+
+    let mut cli = DhcpV4Client::init(config, None).unwrap();
+
+    let lease = get_lease(&mut cli);
+    assert!(lease.is_some());
+    if let Some(lease) = lease {
+        assert_eq!(lease.yiaddr, FOO1_STATIC_IP,);
+    }
+}
+
+#[test]
+fn test_dhcpv4_host_name() {
+    let _srv = DhcpServerEnv::start();
+
+    let mut config = DhcpV4Config::new(TEST_NIC_CLI);
+    config.set_host_name("foo1");
+
     let mut cli = DhcpV4Client::init(config, None).unwrap();
 
     let lease = get_lease(&mut cli);
     assert!(lease.is_some());
     if let Some(lease) = lease {
         assert_eq!(lease.host_name.as_ref(), Some(&"foo1".to_string()));
-        assert_eq!(lease.yiaddr, FOO1_STATIC_IP,);
+    }
+}
+
+#[test]
+fn test_dhcpv4_use_host_name_as_client_id() {
+    let _srv = DhcpServerEnv::start();
+    let mut config = DhcpV4Config::new(TEST_NIC_CLI);
+    config.set_host_name("foo1");
+    config.use_host_name_as_client_id();
+
+    let mut cli = DhcpV4Client::init(config.clone(), None).unwrap();
+
+    let lease = get_lease(&mut cli);
+    let srv_lease = DhcpServerEnv::get_latest_lease();
+
+    assert!(lease.is_some());
+    assert!(srv_lease.is_some());
+
+    if let Some(srv_lease) = srv_lease {
+        assert_eq!(srv_lease.client_id, config.client_id,);
+    }
+}
+
+#[test]
+fn test_dhcpv4_use_mac_as_client_id() {
+    let _srv = DhcpServerEnv::start();
+    let mut config = DhcpV4Config::new(TEST_NIC_CLI);
+    config.use_mac_as_client_id();
+    let mut cli: DhcpV4Client = DhcpV4Client::init(config, None).unwrap();
+
+    let lease = get_lease(&mut cli);
+    let srv_lease = DhcpServerEnv::get_latest_lease();
+    let cli_mac = get_test_veth_cli_mac();
+
+    assert!(lease.is_some());
+    assert!(srv_lease.is_some());
+    assert!(!cli_mac.is_empty());
+
+    if let Some(srv_lease) = srv_lease {
+        assert_eq!(srv_lease.mac, cli_mac);
     }
 }
 
--- src/integ_tests/dhcpv4_async.rs
+++ src/integ_tests/dhcpv4_async.rs
@@ -14,7 +14,7 @@ fn test_dhcpv4_async() {
     config.set_host_name("foo1");
     config.use_host_name_as_client_id();
 
-    let mut cli = DhcpV4ClientAsync::init(config, None).unwrap();
+    let mut cli = DhcpV4ClientAsync::init(config.clone(), None).unwrap();
 
     let rt = tokio::runtime::Builder::new_current_thread()
         .enable_io()
@@ -22,10 +22,14 @@ fn test_dhcpv4_async() {
         .unwrap();
 
     let lease = rt.block_on(get_lease(&mut cli));
+    let srv_lease = DhcpServerEnv::get_latest_lease();
     assert!(lease.is_some());
-    if let Some(lease) = lease {
+    assert!(srv_lease.is_some());
+
+    if let (Some(srv_lease), Some(lease)) = (srv_lease, lease) {
         assert_eq!(lease.host_name.as_ref(), Some(&"foo1".to_string()));
         assert_eq!(lease.yiaddr, FOO1_STATIC_IP,);
+        assert_eq!(srv_lease.client_id, config.client_id,);
     }
 }
 
